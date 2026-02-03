bef6aa6e10521c768d15f008aa389f28780522f4
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-02-03 17:18:13 +0300

    Fix loading state in Home & error state in AddPost

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 import axios from "axios";
 
 function Posts() {
-  const [articles, setArticles] = useState([]);
+  const [articles, setArticles] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -32,7 +32,7 @@ function Posts() {
             <h2 style={{ fontSize: "20px" }}>Articles</h2>
             <Link to='/add-post'><img src="/plus-icon.svg" alt="" width='24px' /></Link>
         </div>
-        {isLoading ? (
+        {!articles ? (
           <h3 style={{ textAlign: "center" }}>Loading articles...</h3>
         ) : error ? (
           <h3 style={{ textAlign: "center" }}>{error}</h3>
--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -4,24 +4,29 @@ import { Link, useNavigate } from "react-router";
 import axios from "axios";
 
 function AddPost() {
-    const navigate = useNavigate()
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('')
+  const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [error, setError] = useState(null);
 
   async function handelAddPost() {
     try {
-      const response = await axios.post('https://jsonplaceholder.typicode.com/posts', {
-        userId: 11,
-        title: title,
-        body: body
-      }, {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-      })
-      navigate('/')
+      const response = await axios.post(
+        "https://jsonplaceholder.typicode.com/posts",
+        {
+          userId: 11,
+          title: title,
+          body: body,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      );
+      navigate("/");
     } catch (err) {
-      console.log(err);
+      setError(err.message);
     }
   }
   return (
@@ -52,6 +57,10 @@ function AddPost() {
               }}
             ></textarea>
           </div>
+          {
+            error? <h3 style={{textAlign: 'center', marginTop: '20px'}}>{error}</h3>
+            : <></>
+          }
         </section>
       </div>
     </>
