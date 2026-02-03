f2ee7240fd985aa2baa9ede3d04a3940c87754d2
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-02-03 17:09:06 +0300

    Add comments section and fix loading state in post

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -6,9 +6,13 @@ import Hero from "../components/Hero.jsx";
 function Post() {
   const id = useParams().id;
 
-  const [postObj, setPostObj] = useState({});
+  const [postObj, setPostObj] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  const [comments, setComments] = useState(null);
+  const [commentsIsLoading, setCommentsIsLoading] = useState(true);
+  const [commentsError, setCommentsError] = useState(null);
 
   async function fetchPost() {
     try {
@@ -21,32 +25,71 @@ function Post() {
     }
   }
 
+  async function fetchComments() {
+    try {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/posts/${id}/comments`,
+      );
+      setComments(response.data);
+    } catch (err) {
+      setCommentsError(err);
+    }
+  }
+
   useEffect(() => {
     if (isLoading) {
       fetchPost();
       setIsLoading(false);
     }
+    if (commentsIsLoading) {
+      fetchComments();
+      setCommentsIsLoading(false);
+    }
+
+    return () => {
+      setIsLoading(true);
+      setCommentsIsLoading(true);
+    };
   }, []);
 
   return (
-    <>
+    <main>
       <Hero />
       <div className="container">
         <section className="posts">
-        <Link to='/' ><img src="/left-arrow.svg" alt="" /></Link>
-          {isLoading ? (
-            <h3 style={{ textAlign: "center" }}>Loading articles...</h3>
+          <Link to="/">
+            <img src="/left-arrow.svg" alt="" />
+          </Link>
+          {!postObj ? (
+            <h3 style={{ textAlign: "center" }}>Loading article...</h3>
           ) : error ? (
             <h3 style={{ textAlign: "center" }}>{error}</h3>
           ) : (
             <>
-            <h2 style={{ fontSize: "20px" }}>{postObj.title}</h2>
-            <p>{postObj.body}</p>
+              <h2 style={{ fontSize: "20px" }}>{postObj.title}</h2>
+              <p>{postObj.body}</p>
+              <hr style={{ margin: "20px 0" }} />
+              <section className="comments">
+                {!comments ? (
+                  <h3 style={{ textAlign: "center" }}>Loading articles...</h3>
+                ) : commentsError ? (
+                  <h3 style={{ textAlign: "center" }}>{error}</h3>
+                ) : (
+                  comments.map((comment) => {
+                    return (
+                      <div key={comment.id} className="comment">
+                        <h3>{comment.name}</h3>
+                        <p>{comment.body}</p>
+                      </div>
+                    );
+                  })
+                )}
+              </section>
             </>
           )}
         </section>
       </div>
-    </>
+    </main>
   );
 }
 
